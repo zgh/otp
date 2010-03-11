864091a37030eac122b4029377c05985338586fb
Erik Rigtorp <erik@rigtorp.com>
2010-03-11 13:20:45 +0100

    Add function to decode distribution header
    There is currently no way to use erl_interface with non-blocking
    sockets. This patch adds a function ei_decode_header() that can decode
    the distribution header of a message sent to a C-node. This will allow a
    C-node to receive messages using an asynchronous event loop.

--- lib/erl_interface/src/connect/eirecv.c
+++ lib/erl_interface/src/connect/eirecv.c
@@ -42,11 +42,147 @@
 
 #define EIRECVBUF 2048 /* largest possible header is approx 1300 bytes */
 
-/* length (4), PASS_THOUGH (1), header, message */
+
 int 
-ei_recv_internal (int fd, 
-		  char **mbufp, int *bufsz, 
-		  erlang_msg *msg, int *msglenp, 
+ei_decode_header_internal(const char *buf, int *index, erlang_msg *msg)
+{
+  char *s;
+  int arity;
+  int version;
+  int show_this_msg = 0;
+
+  /* pass-through, version, control tuple header, control message type */
+  s = buf + *index;
+  (*index)++;
+  if ((get8(s) != ERL_PASS_THROUGH)
+      || ei_decode_version(buf,index,&version)
+      || (version != ERL_VERSION_MAGIC) 
+      || ei_decode_tuple_header(buf,index,&arity)
+      || ei_decode_long(buf,index,&msg->msgtype))
+  {
+      erl_errno = EIO;	/* Maybe another code for decoding errors */
+      return -1;
+  }
+  
+  switch (msg->msgtype) {
+  case ERL_SEND:          /* { SEND, Cookie, ToPid } */
+    if (ei_tracelevel > 0) show_this_msg = 1;
+    if (ei_decode_atom(buf,index,msg->cookie)
+	|| ei_decode_pid(buf,index,&msg->to))
+    {
+	erl_errno = EIO;
+	return -1;
+    }
+
+    break;
+
+  case ERL_REG_SEND:     /* { REG_SEND, From, Cookie, ToName } */
+    if (ei_tracelevel > 0) show_this_msg = 1;
+    if (ei_decode_pid(buf,index,&msg->from)
+	|| ei_decode_atom(buf,index,msg->cookie)
+	|| ei_decode_atom(buf,index,msg->toname))
+    {
+	erl_errno = EIO;
+	return -1;
+    }
+
+    /* actual message is remaining part of bufbuf, plus any unread bytes */
+    break;
+
+  case ERL_LINK:         /* { LINK, From, To } */
+  case ERL_UNLINK:       /* { UNLINK, From, To } */
+  case ERL_GROUP_LEADER: /* { GROUP_LEADER, From, To } */
+    if (ei_tracelevel > 1) show_this_msg = 1;
+    if (ei_decode_pid(buf,index,&msg->from)
+	|| ei_decode_pid(buf,index,&msg->to))
+    {
+	erl_errno = EIO;
+	return -1;
+    }
+
+    break;
+    
+  case ERL_EXIT:         /* { EXIT, From, To, Reason } */
+  case ERL_EXIT2:        /* { EXIT2, From, To, Reason } */
+    if (ei_tracelevel > 1) show_this_msg = 1;
+    if (ei_decode_pid(buf,index,&msg->from)
+	|| ei_decode_pid(buf,index,&msg->to))
+    {
+	erl_errno = EIO;
+	return -1;
+    }
+
+    break;
+    
+  case ERL_SEND_TT:      /* { SEND_TT, Cookie, ToPid, TraceToken } */
+    if (ei_tracelevel > 0) show_this_msg = 1;
+    if (ei_decode_atom(buf,index,msg->cookie)
+	|| ei_decode_pid(buf,index,&msg->to)
+	|| ei_decode_trace(buf,index,&msg->token))
+    {
+	erl_errno = EIO;
+	return -1;
+    }
+
+    break;
+
+  case ERL_REG_SEND_TT: /* { REG_SEND_TT, From, Cookie, ToName, TraceToken } */
+    if (ei_tracelevel > 0) show_this_msg = 1;
+    if (ei_decode_pid(buf,index,&msg->from)
+	|| ei_decode_atom(buf,index,msg->cookie)
+	|| ei_decode_atom(buf,index,msg->toname)
+	|| ei_decode_trace(buf,index,&msg->token))
+    {
+	erl_errno = EIO;
+	return -1;
+    }
+
+    break;
+
+  case ERL_EXIT_TT:     /* { EXIT_TT, From, To, TraceToken, Reason } */
+  case ERL_EXIT2_TT:    /* { EXIT2_TT, From, To, TraceToken, Reason } */
+    if (ei_tracelevel > 1) show_this_msg = 1;
+    if (ei_decode_pid(buf,index,&msg->from)
+	|| ei_decode_pid(buf,index,&msg->to)
+	|| ei_decode_trace(buf,index,&msg->token))
+    {
+	erl_errno = EIO;
+	return -1;
+    }
+
+    break;
+
+  case ERL_NODE_LINK:   /* { NODE_LINK } */
+    if (ei_tracelevel > 1) show_this_msg = 1;
+    break;
+
+  default:
+    /* unknown type, just put any remaining bytes into buffer */
+    break;
+  }
+
+  return show_this_msg;
+}
+
+int
+ei_decode_header(const char *buf, int *index, erlang_msg *msg)
+{
+  if (ei_decode_header_internal(buf, index, msg) == -1) {
+    return -1;
+  }
+
+  /* the caller only sees "untraced" message types */
+  /* the trace token is buried in the message struct */
+  if (msg->msgtype > 10) msg->msgtype -= 10;
+
+  return msg->msgtype;
+}
+
+/* length (4), PASS_THOUGH (1), header, message */
+int
+ei_recv_internal (int fd,
+		  char **mbufp, int *bufsz,
+		  erlang_msg *msg, int *msglenp,
 		  int staticbufp, unsigned ms)
 {
   char header[EIRECVBUF];
@@ -56,15 +192,13 @@ ei_recv_internal (int fd,
   int msglen = 0;
   int bytesread = 0;
   int remain;
-  int arity;
-  int version;
   int index = 0;
   int i = 0;
   int res;
   int show_this_msg = 0;
 
   /* get length field */
-  if ((res = ei_read_fill_t(fd, header, 4, ms)) != 4) 
+  if ((res = ei_read_fill_t(fd, header, 4, ms)) != 4)
   {
       erl_errno = (res == -2) ? ETIMEDOUT : EIO;
       return -1;
@@ -78,132 +212,22 @@ ei_recv_internal (int fd,
     *msglenp = 0;
     return 0;			/* maybe flag ERL_EAGAIN [sverkerw] */
   }
-  
+
   /* turn off tracing on each receive. it will be turned back on if
    * we receive a trace token.
    */
   ei_trace(-1,NULL);
-  
+
   /* read enough to get at least entire header */
-  bytesread = (len > EIRECVBUF ? EIRECVBUF : len); 
+  bytesread = (len > EIRECVBUF ? EIRECVBUF : len);
   if ((i = ei_read_fill_t(fd,header,bytesread,ms)) != bytesread) {
       erl_errno = (i == -2) ? ETIMEDOUT : EIO;
       return -1;
   }
 
   /* now decode header */
-  /* pass-through, version, control tuple header, control message type */
-  s = header;
-  index = 1;
-  if ((get8(s) != ERL_PASS_THROUGH)
-      || ei_decode_version(header,&index,&version)
-      || (version != ERL_VERSION_MAGIC) 
-      || ei_decode_tuple_header(header,&index,&arity) 
-      || ei_decode_long(header,&index,&msg->msgtype))
-  {
-      erl_errno = EIO;	/* Maybe another code for decoding errors */
-      return -1;
-  }
-  
-  switch (msg->msgtype) {
-  case ERL_SEND:          /* { SEND, Cookie, ToPid } */
-    if (ei_tracelevel > 0) show_this_msg = 1;
-    if (ei_decode_atom(header,&index,msg->cookie) 
-	|| ei_decode_pid(header,&index,&msg->to))
-    {
-	erl_errno = EIO;
-	return -1;
-    }
-
-    break;
-
-  case ERL_REG_SEND:     /* { REG_SEND, From, Cookie, ToName } */
-    if (ei_tracelevel > 0) show_this_msg = 1;
-    if (ei_decode_pid(header,&index,&msg->from) 
-	|| ei_decode_atom(header,&index,msg->cookie) 
-	|| ei_decode_atom(header,&index,msg->toname))
-    {
-	erl_errno = EIO;
-	return -1;
-    }
-
-    /* actual message is remaining part of headerbuf, plus any unread bytes */
-    break;
-
-  case ERL_LINK:         /* { LINK, From, To } */
-  case ERL_UNLINK:       /* { UNLINK, From, To } */
-  case ERL_GROUP_LEADER: /* { GROUP_LEADER, From, To } */
-    if (ei_tracelevel > 1) show_this_msg = 1;
-    if (ei_decode_pid(header,&index,&msg->from) 
-	|| ei_decode_pid(header,&index,&msg->to))
-    {
-	erl_errno = EIO;
-	return -1;
-    }
-
-    break;
-    
-  case ERL_EXIT:         /* { EXIT, From, To, Reason } */
-  case ERL_EXIT2:        /* { EXIT2, From, To, Reason } */
-    if (ei_tracelevel > 1) show_this_msg = 1;
-    if (ei_decode_pid(header,&index,&msg->from) 
-	|| ei_decode_pid(header,&index,&msg->to))
-    {
-	erl_errno = EIO;
-	return -1;
-    }
-
-    break;
-    
-  case ERL_SEND_TT:      /* { SEND_TT, Cookie, ToPid, TraceToken } */
-    if (ei_tracelevel > 0) show_this_msg = 1;
-    if (ei_decode_atom(header,&index,msg->cookie) 
-	|| ei_decode_pid(header,&index,&msg->to)
-	|| ei_decode_trace(header,&index,&msg->token))
-    {
-	erl_errno = EIO;
-	return -1;
-    }
-
-    ei_trace(1,&msg->token); /* turn on tracing */
-    break;
-
-  case ERL_REG_SEND_TT: /* { REG_SEND_TT, From, Cookie, ToName, TraceToken } */
-    if (ei_tracelevel > 0) show_this_msg = 1;
-    if (ei_decode_pid(header,&index,&msg->from) 
-	|| ei_decode_atom(header,&index,msg->cookie) 
-	|| ei_decode_atom(header,&index,msg->toname)
-	|| ei_decode_trace(header,&index,&msg->token))
-    {
-	erl_errno = EIO;
-	return -1;
-    }
-
-    ei_trace(1,&msg->token); /* turn on tracing */
-    break;
-
-  case ERL_EXIT_TT:     /* { EXIT_TT, From, To, TraceToken, Reason } */
-  case ERL_EXIT2_TT:    /* { EXIT2_TT, From, To, TraceToken, Reason } */
-    if (ei_tracelevel > 1) show_this_msg = 1;
-    if (ei_decode_pid(header,&index,&msg->from) 
-	|| ei_decode_pid(header,&index,&msg->to)
-	|| ei_decode_trace(header,&index,&msg->token))
-    {
-	erl_errno = EIO;
-	return -1;
-    }
-
-    ei_trace(1,&msg->token); /* turn on tracing */
-    break;
-
-  case ERL_NODE_LINK:   /* { NODE_LINK } */
-    if (ei_tracelevel > 1) show_this_msg = 1;
-    break;
-
-  default:
-    /* unknown type, just put any remaining bytes into buffer */
-    break;
-  }
+  if ((show_this_msg = ei_decode_header_internal(header, &index, msg)) == -1)
+    return -1;
 
   /* actual message is remaining part of headerbuf, plus any unread bytes */
   msglen = len - index;     /* message size (payload) */
@@ -262,8 +286,11 @@ ei_recv_internal (int fd,
 
   /* the caller only sees "untraced" message types */
   /* the trace token is buried in the message struct */
-  if (msg->msgtype > 10) msg->msgtype -= 10;
-  
+  if (msg->msgtype > 10) {
+    ei_trace(1,&msg->token); /* turn on tracing */
+    msg->msgtype -= 10;
+  }
+
   return msg->msgtype;
 }
 
